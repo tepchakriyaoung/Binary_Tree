a5dbb79314265213157710c8a77ba315fcd20fb9
Tepchakriya Oung <oungtepchakriya@gmail.com>
2026-08-27 12:19:50 +1000

    Adding the com file and spira test config and enter data

--- src/test/java/BTTest.java
+++ src/test/java/BTTest.java
@@ -5,6 +5,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import com.inflectra.spiratest.addons.junitextension.SpiraTestCase;
+import com.inflectra.spiratest.addons.junitextension.SpiraTestConfiguration;
+
+@SpiraTestConfiguration (
+//following are REQUIRED
+        url = "https://rmit.spiraservice.net/",
+        login = "S4064858",
+        rssToken = "{83B01DDE-65A1-4AC4-8DEE-7C5FE74D95CD}",
+        projectId = 1216,
+//following are OPTIONAL
+        releaseId = 7,
+        testSetId = 1
+)
 
 class BTTest {
 
